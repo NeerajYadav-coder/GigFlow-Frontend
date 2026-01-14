307d0e744257bb92fbbbe879c261265e36332aff
NeerajYadav-coder <neerajbhaiya1508@gmail.com>
2026-01-14 14:00:31 +0530

    Final-fixed

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,10 +1,27 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import api from "../api/axios";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  /* 🔁 Restore login from cookie */
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await api.get("/auth/me");
+        setUser(res.data.user);
+      } catch {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   const register = async (formData) => {
     const res = await api.post("/auth/register", formData);
@@ -22,8 +39,8 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, register, login, logout }}>
-      {children}
+    <AuthContext.Provider value={{ user, loading, register, login, logout }}>
+      {!loading && children}
     </AuthContext.Provider>
   );
 };
